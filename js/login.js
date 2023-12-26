39297b3dcc7f04d59cd5f49731c2145769c61b81
hexie2108 <hexie2109@gmail.com>
2023-12-26 18:40:50 +0100

    修复登陆页的CSS问题

--- js/login.js
+++ js/login.js
@@ -1,15 +1,22 @@
 
 /*$(function () {
 */
-
+const LOGIN_SITE_DOMAIN = {
+    www_mikuclub_online: 'www.mikuclub.online',
+    www_mikuclub_cc: 'www.mikuclub.cc',
+    www_mikuclub_win: 'www.mikuclub.win',
+    www_mikuclub_eu: 'www.mikuclub.eu',
+    www_mikuclub_uk: 'www.mikuclub.uk',
+  
+}
 
 
 //如果当前域名为 online 或者 cc
-if (location.host === SITE_DOMAIN.www_mikuclub_online || location.host === SITE_DOMAIN.www_mikuclub_cc) {
+if (location.host === LOGIN_SITE_DOMAIN.www_mikuclub_online || location.host === LOGIN_SITE_DOMAIN.www_mikuclub_cc) {
     //重定向到win
     let url = location.href
-        .replace(SITE_DOMAIN.www_mikuclub_online, SITE_DOMAIN.www_mikuclub_win)
-        .replace(SITE_DOMAIN.www_mikuclub_cc, SITE_DOMAIN.www_mikuclub_online);
+        .replace(LOGIN_SITE_DOMAIN.www_mikuclub_online, LOGIN_SITE_DOMAIN.www_mikuclub_win)
+        .replace(LOGIN_SITE_DOMAIN.www_mikuclub_cc, LOGIN_SITE_DOMAIN.www_mikuclub_online);
     location.replace(url);
 }
 
