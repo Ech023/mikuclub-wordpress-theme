be28268d2e83b06ddab127a192edf03a24aa384c
hexie2108 <hexie2109@gmail.com>
2023-12-26 15:22:07 +0100

    切换使用本地fontawesome库

--- functions.php
+++ functions.php
@@ -39,10 +39,15 @@ function setup_front_end_external_css_and_script()
     //wp_enqueue_script('vue-3', 'https://cdn.staticfile.org/vue/3.3.4/vue.global.prod.min.js', [], '3.3.4', false);
 
     //fontanwesome图标库
-    wp_enqueue_style('fontawesome', 'https://cdn.staticfile.org/font-awesome/6.4.2/css/fontawesome.min.css', [], '5.13');
-    wp_enqueue_style('fontawesome-solid', 'https://cdn.staticfile.org/font-awesome/6.4.2/css/solid.min.css', [], '5.13');
-    wp_enqueue_style('fontawesome-brand', 'https://cdn.staticfile.org/font-awesome/6.4.2/css/brands.min.css', [], '5.13');
-    //wp_enqueue_style('fontawesome-regular', 'https://cdn.staticfile.org/font-awesome/6.3.0/css/regular.min.css', false, '5.13');
+    // wp_enqueue_style('fontawesome', 'https://cdn.staticfile.org/font-awesome/6.4.2/css/fontawesome.min.css', [], '5.13');
+    // wp_enqueue_style('fontawesome-solid', 'https://cdn.staticfile.org/font-awesome/6.4.2/css/solid.min.css', [], '5.13');
+    // wp_enqueue_style('fontawesome-brand', 'https://cdn.staticfile.org/font-awesome/6.4.2/css/brands.min.css', [], '5.13');
+    //fontanwesome图标库 本地版本
+    $template_directory_uri = get_template_directory_uri();
+    wp_enqueue_style('fontawesome', $template_directory_uri . '/css/font-awesome/6.5.1/css/fontawesome.min.css', [], '6.5.1');
+    wp_enqueue_style('fontawesome-solid', $template_directory_uri . '/css/font-awesome/6.5.1/css/solid.min.css', [], '6.5.1');
+    wp_enqueue_style('fontawesome-brand', $template_directory_uri . '/css/font-awesome/6.5.1/css/brands.min.css', [], '6.5.1');
+
 
     //bootstrap库
     wp_enqueue_style('twitter-bootstrap-css', 'https://cdn.staticfile.org/twitter-bootstrap/5.3.2/css/bootstrap.min.css', [], '5.3.2');
@@ -115,9 +120,11 @@ function setup_front_end_css()
         ],
     ];
 
+    $template_directory_uri = get_template_directory_uri();
+
     foreach ($custom_styles as $style)
     {
-        wp_enqueue_style($style['name'], get_template_directory_uri() . $style['path'], [], Config::CSS_JS_VERSION);
+        wp_enqueue_style($style['name'], $template_directory_uri . $style['path'], [], Config::CSS_JS_VERSION);
     }
 }
 
@@ -333,9 +340,11 @@ function setup_front_end_script()
 
     ];
 
+    $template_directory_uri = get_template_directory_uri();
+
     foreach ($custom_scripts as $script)
     {
-        wp_enqueue_script($script['name'], get_template_directory_uri() . $script['path'], [], Config::CSS_JS_VERSION, $script['in_footer']);
+        wp_enqueue_script($script['name'], $template_directory_uri . $script['path'], [], Config::CSS_JS_VERSION, $script['in_footer']);
     }
 }
 
