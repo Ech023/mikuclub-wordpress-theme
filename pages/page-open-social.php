60d165f97f167065507df7f8a8d90fcacb571872
hexie2108 <hexie2109@gmail.com>
2024-01-14 15:38:20 +0100

    更新 open social 模板页

--- pages/page-open-social.php
+++ pages/page-open-social.php
@@ -4,6 +4,9 @@ template name: OPEN-SOCIAL 社会化登陆页
 description: 用来 处理 OPEN-SOCIAL 登陆/绑定/解绑等操作的页面
 */
 
+namespace mikuclub;
 
-wp_redirect(get_home_url());
+do_action('open_social_init');
+
+// wp_redirect(get_home_url());
 exit;
